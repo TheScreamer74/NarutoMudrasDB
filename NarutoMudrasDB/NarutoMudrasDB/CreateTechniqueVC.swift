86a62ee480b999a66693c2c32774f52757607c38
Louis <louis-lyonnais@live.fr>
2021-03-31 11:37:35 +0200

    ft/add_technique

---
+++ NarutoMudrasDB/NarutoMudrasDB/CreateTechniqueVC.swift
@@ -0,0 +1,42 @@
+//
+//  CreateTechniqueVC.swift
+//  NarutoMudrasDB
+//
+//  Created by Louis Jeanty on 31/03/2021.
+//
+
+import UIKit
+
+class CreateTechniqueVC: UIViewController {
+    @IBOutlet weak var navBarRightButton: UIButton!
+    @IBOutlet weak var addImageButton: UIButton!
+    
+    @IBOutlet weak var techniqueImage: UIImageView!
+    
+    @IBAction func navBarRightAction(_ sender: Any) {
+    }
+    
+    @IBAction func addImageAction(_ sender: Any) {
+    }
+    
+    @IBAction func addSigneAction(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
